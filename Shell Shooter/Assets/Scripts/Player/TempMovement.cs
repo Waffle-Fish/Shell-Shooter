5e6befc9af857524816ec13c8215ec7840f74941
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-08-30 14:02:42 -0700

    add player movement

--- Shell Shooter/Assets/Scripts/Player/TempMovement.cs
+++ Shell Shooter/Assets/Scripts/Player/TempMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class TempMovement : MonoBehaviour
+public class TempMovement : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (!IsOwner) { return; }
+
+        Vector3 moveDir = new(0,0,0);
+        if (Input.GetKey(KeyCode.W)) moveDir.y = 1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.y = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = 1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveSpeed * Time.deltaTime * moveDir;
     }
 }
