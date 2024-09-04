ebb83da01318df731ed06a080001bc60b2099ba1
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-09-03 20:14:05 -0700

    start on lobby system

---
+++ Shell Shooter/Assets/Scripts/Network/Lobby/TestLobby.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class TestLobby : MonoBehaviour
+{
+    private Lobby hostLobby;
+    private float heartbeatTimer;
+    private async void Start() {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () => {
+            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private void Update() {
+        HandleLobbyHeartbeat();
+    }
+
+    private async void HandleLobbyHeartbeat()
+    {
+        if (hostLobby != null) {
+            heartbeatTimer -= Time.deltaTime;
+            if (heartbeatTimer < 0f) {
+                float heartbeatTimerMax = 15f; // The server goes inactive every 30 seconds
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
+    [Button("Create Lobby")]
+    private async void CreateLobby() {
+        try {
+            string lobbyName = "MyLobby";
+            int maxPlayers = 4;
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+
+            hostLobby = lobby;
+            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
+        } catch (LobbyServiceException e) {
+            Debug.LogError(e);
+        }
+    }
+
+    [Button("List Lobbies")]
+    private async void ListLobbies() {
+        try {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions() {
+                Count = 25,
+                Filters = new List<QueryFilter> {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder> {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+            
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync(queryLobbiesOptions);
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results) {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        } catch (LobbyServiceException e) {
+            Debug.LogError(e);
+        }
+    }
+
+}
