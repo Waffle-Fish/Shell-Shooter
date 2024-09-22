54606c239c2dd094db3ea62dd4b8929ee29caf3b
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-09-21 23:10:22 -0700

    add movement

---
+++ Shell Shooter/Assets/Scripts/Player/Controls/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce;
+    PlayerControls playerControls;
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    private void Awake() {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void OnDisable() {
+        playerControls.Disable();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMovement();
+    }
+
+    private void ProcessMovement() {
+        Debug.Log(GetInput());
+        rb.AddForce(GetInput() * moveForce, ForceMode2D.Force);
+    }
+
+    private Vector2 GetInput() {
+        return new Vector2(playerControls.Movement.Horizontal.ReadValue<float>(), playerControls.Movement.Vertical.ReadValue<float>()).normalized;
+    }
+}
