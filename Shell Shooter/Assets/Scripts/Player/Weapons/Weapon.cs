2b114c661b5e53832f83cc47c9a795b404d6369d
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-09-26 18:26:59 -0700

    start on weapon

---
+++ Shell Shooter/Assets/Scripts/Player/Weapons/Weapon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    // Damage per hit or Damage per second
+    protected float Damage;
+    // Time between consecutive uses of this attack
+    protected float Cooldown;
+
+    abstract public void ProcessAttack();
+
+}
